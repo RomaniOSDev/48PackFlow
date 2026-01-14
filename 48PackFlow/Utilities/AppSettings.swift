f42c45374956e43ce2c7828474848e0c51df06e7
Roman <romazan.26@gmail.com>
2026-01-14 20:47:47 +0300

    final

--- 48PackFlow/Utilities/AppSettings.swift
+++ 48PackFlow/Utilities/AppSettings.swift
@@ -11,13 +11,13 @@ import StoreKit
 
 struct AppSettings {
     static func openPrivacyPolicy() {
-        if let url = URL(string: "https://example.com/privacy-policy") {
+        if let url = URL(string: "https://www.termsfeed.com/live/bebd3374-e003-4151-872a-c8f2264c4d7c") {
             UIApplication.shared.open(url)
         }
     }
     
     static func openTermsOfService() {
-        if let url = URL(string: "https://example.com/terms-of-service") {
+        if let url = URL(string: "https://www.termsfeed.com/live/16d79553-f3a2-4b05-9d05-e2314be68df1") {
             UIApplication.shared.open(url)
         }
     }
